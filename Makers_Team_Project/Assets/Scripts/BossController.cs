34486f39306263b0c2f51494ab0448d876f20996
[hsb031106@naver.com] <[hsb031106@naver.com]>
2022-11-22 00:53:04 +0900

    Revert "보스 수정"
    This reverts commit 8f52903e21199ff8c2402d0507fcf058d803df48.

--- Makers_Team_Project/Assets/Scripts/BossController.cs
+++ Makers_Team_Project/Assets/Scripts/BossController.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class BossController : MonoBehaviour
 {
     [SerializeField] GameObject Bullet;
-    [SerializeField] GameObject Enemy1;
-    [SerializeField] GameObject Enemy2;
+    [SerializeField] GameObject Enermy;
     [SerializeField] GameObject Laser;
     Rigidbody2D rigid;
-    public int HP = 100;
     public float speed = 0.1f;
     private float LaserDelay = 15f;
     private float curLaserCool = 1f;
@@ -23,9 +21,6 @@ public class BossController : MonoBehaviour
     private Transform BulletTransform;
     private Transform EnermyTransform;
     private Transform LaserTransform;
-    private int random;
-    private int random_subEnemy;
-    
 
     void Start()
     {
@@ -33,39 +28,25 @@ public class BossController : MonoBehaviour
         BulletTransform = this.gameObject.transform.GetChild(0);
         EnermyTransform = this.gameObject.transform.GetChild(1);
         LaserTransform = this.gameObject.transform.GetChild(2);
-        random = Random.Range(0, 3);
-       
 
     }
 
     void Update()
     {
-        if (random == 0)
-            shootLaser();
+        shootLaser();
+        Attack();
+        shootEnermy();
 
-        if (random == 1)
-            Attack();
-
-        if (random == 2)
-            shootEnemy();
-
-        if (isLaserReady)
+        if(isLaserReady)
         {
             Destroy(GameObject.Find("Tmp_subEnermy(Clone)"));
             Destroy(GameObject.Find("Tmp_BossBullet(Clone)"));
         }
-
-        if (HP <= 0)
-            Destroy(this.gameObject);
-
-        
-
     }
     private void Attack()
     {
         curFireCool += Time.deltaTime;
         isFireReady = curFireCool > fireDelay;
-
 
         if(isLaserReady)
         {
@@ -78,32 +59,26 @@ public class BossController : MonoBehaviour
             curFireCool = 0f;
         }
 
+
+
+
     }
 
-    private void shootEnemy()
+    private void shootEnermy()
     {
         curShootCool += Time.deltaTime;
         isShootReady = curShootCool > shootDelay;
-        random_subEnemy = Random.Range(0, 2);
 
-        if (isLaserReady)
+
+        if(isLaserReady)
         {
             curShootCool = 0f;
         }
 
         if (isShootReady)
         {
-            if(random_subEnemy == 0)
-            {
-                Instantiate(Enemy1, EnermyTransform.position, EnermyTransform.rotation);
-                curShootCool = 0f;
-            }
-            else
-            {
-                Instantiate(Enemy2, EnermyTransform.position, EnermyTransform.rotation);
-                curShootCool = 0f;
-            }
-
+            Instantiate(Enermy, EnermyTransform.position, EnermyTransform.rotation);
+            curShootCool = 0f;
         }
 
     }
@@ -115,27 +90,16 @@ public class BossController : MonoBehaviour
         if(isLaserReady)
         {
             Destroy(GameObject.Find("Tmp_subEnermy(Clone)"));
-
             Instantiate(Laser, LaserTransform.position, LaserTransform.rotation);
-
             Destroy(GameObject.Find("Tmp_subEnermy(Clone)"));
  
             isShootReady = false;
             isFireReady = false;
-
             Destroy(GameObject.Find("Tmp_subEnermy(Clone)"));
             Destroy(GameObject.Find("Tmp_Laser(Clone)"), 3f);
-
             curLaserCool = 0f;
         }
+        
     }
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "p_Bullet")
-        {
-            HP -= 10;
-        }
-    }
+    
 }
